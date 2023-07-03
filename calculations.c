09f1f1e54fb720ca31668759d1a539b6de4cd625
Dilshan09 <121423710+Dilshan09@users.noreply.github.com>
2023-07-03 12:36:17 +0530

    Calculations
    Calculations

---
+++ calculations.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+ 
+ 
+int main()
+{
+	
+	printf("Addition : %d\n", 1+2);
+	printf("%d\n",4+5); 
+	
+	printf("divission : %d\n", 7-5); 
+	printf("divission : %d\n", 4-9); 
+	printf("%d\n",4-1); 
+	
+	printf("multiplication : %d\n",4*8); 
+	printf("%d\n",4*2);  
+	printf("%f\n",4.2*4.2);
+	
+	printf("division : %d\n",8/2); 
+	printf("division : %d\n",2/8); 
+	printf("%d\n",4/2);
+	printf("%f\n",1.2/4.5); 
+	
+	printf("remainder : %d\n",4%5);
+	printf("%d\n",4%2);
+	
+
+	
+	
+	
+	return 0;
+}
