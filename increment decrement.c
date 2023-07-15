e0783826fb55b3ab5b07daa8d1495d24e90af1ea
Dilshan09 <121423710+Dilshan09@users.noreply.github.com>
2023-07-15 09:38:01 +0530

    Operetors ++ , --
    Operetors ++ , --

---
+++ increment decrement.c
@@ -0,0 +1,30 @@
+#include<stdio.h> 
+
+int main() 
+{
+	
+	int x=5; 
+	int y=3; 
+	int a=1; 
+	int b=8; 
+	
+	
+	printf("%d\n", x++);
+    printf("%d\n\n", x);
+
+	printf("%d\n", ++y);
+    printf("%d\n\n", y);
+
+	
+	printf("%d\n", a--);
+    printf("%d\n\n", a);
+
+	printf("%d\n", --b);
+    printf("%d\n\n", b);
+
+	
+	
+	
+	
+	return 0;
+}
