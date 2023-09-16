4ce9cf12663785ab7136df3166f388447ef6feeb
Dilshan09 <pereradilshan594@gmail.com>
2023-09-16 06:16:20 +0530

    tute6.1
    tute6.1

---
+++ tute6.1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ 
+#include<math.h> 
+
+int main() 
+{
+	
+	printf(" %.2f",floor(7.5));
+	
+	printf("\n %.2f",ceil(0.0));
+	
+	printf("\n%.2f",ceil(-6.4));
+	
+	printf("\n%.2f",log10(100.0));
+	
+	printf("\n%.2f",ceil(floor(-5.5)));
+	
+	return 0;
+}
