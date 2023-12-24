1c77303bb8b3db3ebaee81d533ab06cc65b35123
Dilshan09 <pereradilshan594@gmail.com>
2023-12-24 10:11:04 +0530

    2d
    Practice

---
+++ 2d simple4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main()
+{
+	int one[2][2]={{1,2},{3,4}};
+	int two[2][2]={{5,6},{7,8}};
+	
+    int row,column;
+	
+	printf("The First matrix is :\n");
+	for(row=0;row<2;row++)
+	{
+		for(column=0;column<2;column++)
+		{
+			printf("%d ",one[row][column]);
+		}
+		printf("\n");
+	}
+	printf("The Second matrix is :\n");
+	for(row=0;row<2;row++)
+	{
+		for(column=0;column<2;column++)
+		{
+			printf("%d ",two[row][column]);
+		}
+		printf("\n");
+	}
+	printf("The  multiplication of two matrix is :\n");
+	for(row=0;row<2;row++)
+	{
+		for(column=0;column<2;column++)
+		{
+			printf("%d ",one[row][column]*two[row][column]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
